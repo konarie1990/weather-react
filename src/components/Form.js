9913f6c76a693b58b5ddc680be7ab655f94ec879
Ronald Hajewski <ronald.hajewski@gmail.com>
2019-01-21 20:04:24 -0600

    weather app using React and API

---
+++ src/components/Form.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Form = props => (
+  <form onSubmit={props.getWeather}>
+    <input type="text" name="city" placeholder="City..." />
+    <input type="text" name="country" placeholder="Country..." />
+    <button>Get Weather</button>
+  </form>
+);
+
+export default Form;
+
+// REFACTORED CODE ABOVE
+// class Form extends React.Component {
+//   render() {
+//     return (
+//       <form onSubmit={this.props.getWeather}>
+//         <input type="text" name="city" placeholder="City..." />
+//         <input type="text" name="country" placeholder="Country..." />
+//         <button>Get Weather</button>
+//       </form>
+//     );
+//   }
+// };
+
+// export default Form;
---
+++ src/components/Titles.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Titles = () => (
+  <div>
+    <h1>Weather Finder</h1>
+    <p>Find out temperature conditions and more</p>
+  </div>
+);
+
+export default Titles;
+
+// REFACTORED CODE ABOVE
+// class Titles extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Weather Finder</h1>
+//         <p>Find out temperature conditions and more</p>
+//       </div>
+//     );
+//   }
+// };
+
+// export default Titles;
